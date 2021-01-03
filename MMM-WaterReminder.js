5050d8b7d7be311516e3f00bf32d84460dac2331
gomides <thiagomides@gmail.com>
2021-01-03 10:40:44 -0300

    Organizing the view of the module

--- MMM-WaterReminder.js
+++ MMM-WaterReminder.js
@@ -10,6 +10,11 @@
 
 Module.register("MMM-WaterReminder", {
 
+	reminderController : 0,
+	lastphraseIndex : 0,
+	reminderController : 0,
+	timeLeft : 0,
+
 	defaults: {
 		foo: "Did you have enough water today?",
 		phrases: ["Did you have enough water today?", "Hey buddy, you should have some water!","Drink water, bro.","'Better to have stones in the way than in the Kidneys: Drink water!'",
@@ -17,15 +22,17 @@ Module.register("MMM-WaterReminder", {
 		additionalPhrases: [],
 		startTime: "00:00",
 		endTime: "23:59",
-		messageDuration: 1 * 60 * 1000,
+		messageDuration: 1 * 10 * 1000,
 		animationSpeed: 4 * 1000,
-		reminderFrequency: 30 * 60 * 1000,
+		reminderFrequency: 10 * 1000,
 		classes: "",
 		color: "#fff",
 		idleMessage: "<br/>",
 		logo: true,
+		days: [0,1,2,3,4,5,6],
 		alarm: {
 			status: false, 
+			daysWithAudibleReminder: [],
 			src: "done-for-you.mp3",
 			startTime: "09:00",
 			endTime: "17:00",
@@ -35,54 +42,71 @@ Module.register("MMM-WaterReminder", {
 
 	// Define required scripts.
 	getStyles: function () {
-		return ["font-awesome.css","moment.js"];
+		return ["font-awesome.css"];
 	},
 
+	getScripts: function () {
+		return ["moment.js"];
+	},
 
 	start: function (){
-		if (this.config.logo)	this.logo =  " " + "<p class='xsmall dimmed regular'> <i class=' dimmed fa fa-glass-whiskey'></i>  MMM-Water-Reminder  </p>" 
-		else this.logo = " ";
+		this.logo = " ";
+
+		if (this.config.logo)	this.logo =  " " + "<p class='xsmall dimmed regular'> <i class=' dimmed fa fa-glass-whiskey'></i>  MMM-Water-Reminder  </p>";
 		if (this.config.additionalPhrases) this.config.phrases.push(this.config.additionalPhrases);
-		this.message = this.config.foo + this.logo
-		this.lastphraseIndex = 0 
-		this.remainderController = 0
-		this.timeLeft = 0
-		this.config.reminderFrequency = Math.round(this.config.reminderFrequency/this.config.animationSpeed) 
-		this.format = 'hh:mm'
+		
+		this.message = this.config.foo + this.logo;
+		this.config.reminderFrequency = Math.round(this.config.reminderFrequency/this.config.animationSpeed);
+		this.format = 'hh:mm';
+
+
+		if (this.config.alarm["status"])		this.config.alarm["daysWithAudibleReminder"] = this.config.alarm["daysWithAudibleReminder"] ? this.config.alarm["daysWithAudibleReminder"]: this.config.days;
+		else this.config.alarm["daysWithAudibleReminder"] = [];
 
 
 	},
 
-	audioPlay: function (){
+	notificationReceived: function(notification, payload, sender) {
+		switch(notification) {
+			case "DOM_OBJECTS_CREATED":
 
-		var time = moment(moment(),this.format),
-		beforeTime = moment(this.config.alarm["startTime"], this.format),
-		afterTime = moment(this.config.alarm["endTime"], this.format);
-
-
-		if (time.isBetween(beforeTime, afterTime)) {
-			var audio = document.createElement('audio');
-			audio.style.display = "none";
-			audio.src = this.file("sounds/"+this.config.alarm["src"]);
-			audio.volume = 0.5;
-
-			audio.load();
-			audio.autoplay = true;
-			audio.onended = function(){
-				audio.remove() //Remove when played.
-			};
-		}	
-
+				var timer = setInterval(()=>{
+					this.reminderController++;
+					this.textSwitcher();
+					this.updateDom(this.config.animationSpeed);
+				}, this.config.animationSpeed);
+			break;
+		}
 	},
+
+	reminderSound: function (){
+
+		var audio = document.createElement('audio');
+		audio.style.display = "none";
+		audio.src = this.file("sounds/"+this.config.alarm["src"]);
+		audio.volume = 0.5;
+
+		audio.load();
+		audio.autoplay = true;
+
+		audio.onended = function(){
+			audio.remove() 
+		};
+		
+	},
+
 	getDom: function() {
-		var element = document.createElement("div")
-		element.className = this.config.classes ? this.config.classes: "bright medium light"
-		var subElement = document.createElement("span")
-		subElement.innerHTML = this.message
-		subElement.style.color = this.config.color
-		element.appendChild(subElement)
 
-		return element
+		var element = document.createElement("div");
+		element.className = this.config.classes ? this.config.classes: "bright medium light";
+
+		var subElement = document.createElement("span");
+		subElement.innerHTML = this.message;
+		subElement.style.color = this.config.color;
+		
+		element.appendChild(subElement);
+
+		return element;
 	},
 
 	randomPhrase: function (phrases,timeLeft) {
@@ -90,11 +114,8 @@ Module.register("MMM-WaterReminder", {
 			return phrases[0];
 		}
 
-		if (timeLeft != 0) return  phrases[this.lastphraseIndex];
-		else {
-			var timer = setTimeout(()=>{ if(this.config.alarm["status"]) this.audioPlay() }, this.config.animationSpeed/2)
-		} 
-
+		if (timeLeft != 0) return	phrases[this.lastphraseIndex];
+		
 		var generate = function () {
 			return Math.floor(Math.random() * phrases.length);
 		};
@@ -110,39 +131,50 @@ Module.register("MMM-WaterReminder", {
 		return  phrases[phraseIndex];
 	},
 
+
+	isBetween: function(days,startTime,endTime){
+		
+		var IsValidDay = false;
+		let now = moment();
+
+		for (var i = days.length - 1; i >= 0; i--) {
+			if (now.weekday() == this.config.days[i]) IsValidDay = true;
+		}
+
+		if (!IsValidDay) return 0;
+		
+		time = moment(moment(),this.format);
+		beforeTime = moment(startTime, this.format);
+		afterTime = moment(endTime, this.format);
+			
+		if (time.isBetween(beforeTime, afterTime)) return 1;	
+		
+
+		return 0;
+		
+	},
+
 	textSwitcher: function(){
-		var time = moment(moment(),this.format),
-		beforeTime = moment(this.config.startTime, this.format),
-		afterTime = moment(this.config.endTime, this.format);
+	
+		if ((this.reminderController % this.config.reminderFrequency == 0) && (this.isBetween(this.config.days, this.config.startTime, this.config.endTime))){
 
-
-		if ((this.remainderController % this.config.reminderFrequency == 0) && (time.isBetween(beforeTime, afterTime))){
 		    this.message = this.randomPhrase(this.config.phrases,this.timeLeft) + this.logo
+
+		    if (this.isBetween(this.config.alarm["daysWithAudibleReminder"], this.config.alarm["startTime"], this.config.alarm["endTime"]) && (this.timeLeft == 1)) this.reminderSound();
+
 		    if (this.timeLeft <  Math.round(this.config.messageDuration/this.config.animationSpeed)){
-		    	this.remainderController-- 
-		    	this.timeLeft++
+		    	this.reminderController--;
+		    	this.timeLeft++;
 		    } else {
-		    	this.remainderController = 0
-		    	this.timeLeft = 0
+		    	this.reminderController = 0;
+		    	this.timeLeft = 0;
 		    }
 
 		} else {
-			if (this.config.logo)	this.message =  this.config.idleMessage +  this.config.idleMessage
-			else this.message = this.config.idleMessage
+			if (this.config.logo)	this.message =  this.config.idleMessage +  this.config.idleMessage;
+			else this.message = this.config.idleMessage;
 		}
  	},
 
-	notificationReceived: function(notification, payload, sender) {
-		switch(notification) {
-			case "DOM_OBJECTS_CREATED":
-
-				var timer = setInterval(()=>{
-					this.remainderController++
-					this.textSwitcher()
-					this.updateDom(this.config.animationSpeed)
-				}, this.config.animationSpeed)
-			break
-	}
-},
 })
 		
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ A simple and configurable Water Reminder Module for MagicMirror<sup>2</sup>
 * Clone this repo into `~/MagicMirror/modules` directory.
 * Configure your `~/MagicMirror/config/config.js`:
 
+## Minimal configuration
 ```js
 {
     module: 'MMM-WaterReminder',
@@ -32,15 +33,48 @@ A simple and configurable Water Reminder Module for MagicMirror<sup>2</sup>
     }
 }
 ```
+
+## Personalized configuration
+this is the default configuration defined if you don't define any value
+```js
+{
+    module: 'MMM-WaterReminder',
+    position: 'top_center',
+    config: {
+        foo: "Did you have enough water today?",
+        phrases: ["Did you have enough water today?", "Hey buddy, you should have some water!","Drink water, bro.","'Better to have stones in the way than in the Kidneys: Drink water!'",
+        "It's been such a long time since your last glass of water...","Water! Water! Water!","Are you anxious for the next glass of water?"],
+        additionalPhrases: [],
+        startTime: "00:00",
+        endTime: "23:59",
+        messageDuration: 1 * 60 * 1000, // 1 min showing on the screen
+        animationSpeed: 4 * 1000, // 4 seconds for fading
+        reminderFrequency: 60 * 60 * 1000, // hourly reminder
+        classes: "bright medium light",
+        color: "#fff",
+        idleMessage: "<br/>",
+        logo: true,
+        days: [0,1,2,3,4,5,6],
+        alarm: {
+            status: false, 
+            daysWithAudibleReminder: [1,2,3,4,5],
+            src: "done-for-you.mp3",
+            startTime: "09:00",
+            endTime: "17:00",
+        }
+    }
+}
+```
 ## Config Options
 
 | **Option** | **Default** | **Description** |
 | --- | --- | --- |
 | `foo` | `Did you have enough water today?` | This is the module initialization message
-| `phrases` | `["Did you have enough water today?", "Hey buddy, you should have some water!","Drink water, bro.","'Better to have stones in the way than in the Kidneys: Drink water!'", "It's been such a long time since your last glass of water...","Water! Water! Water!","Are you anxious for the next glass of water?"]` | An array of phrases. 
+| `phrases` | `["Did you have enough water today?", ... , "Are you anxious for the next glass of water?"]` | An array of phrases that will be randomly displayed. 
 | `additionalPhrases` | `[]` | An array of phrases to be used with the default array of phrases. 
-| `startTime` | `00:00` | Module start time. 
-| `endTime` | `23:59` | Module end time. 
+| `days` | Days of the week that the reminder should work . <br/>``0 => Sunday, 1 => Monday, 2 => Tuesday, 3 => Wednesday, 4 => Thursday, 5 => Friday, 6 => Saturday``<br/><br/> **Example (workdays) :** ``[1, 2, 3, 4, 5], // From Monday to Friday .`` |
+| `startTime` | `00:00` | The time that the module will start to work. 
+| `endTime` | `23:59` | The time that the module will stop to work. 
 | `messageDuration` | `60000` (1 min) |  How long does the reminder should remain on the screen?  
 | `animationSpeed` | `4000` (4 sec) | Speed of the update animation. 
 | `reminderFrequency` | `1800000` (30 min) | How often does the reminder should appear?
@@ -50,18 +84,20 @@ A simple and configurable Water Reminder Module for MagicMirror<sup>2</sup>
 | `logo` | `true` | If you are using a touch screen device you need to press a button to disable an alarm.. |
 
 
-## Alarm Options
+##  Audible Reminder Options
 
-| **Option**| **Description** |
-| --- | --- |
-| `startTime` | Time at which the alarm should start to sound. e.g., sound start working hours |
-| `endTime` | Time at which the alarm should sound. |
-| `days` | Days of the week. <br/>``0 => Sunday, 1 => Monday, 2 => Tuesday, 3 => Wednesday, 4 => Thursday, 5 => Friday, 6 => Saturday``<br/><br/>**Example :** ``[1, 2, 3, 4, 5], // From Monday to Friday.`` |
-| `sound` | Name or the url of the mp3 file. By default, the sound of the config will be use if this option is empty. |
+| **Option** | **Default** | **Description** |
+| --- | --- | --- |
+| `status` | `false` | Enable or disable audible reminder |
+| `startTime` | `09:00` | Time at which the reminder should start to sound. e.g.,  reminder should sound only during working hours. |
+| `endTime` | `17:00` | Time at which the reminder should stop to sound. e.g., time to get off from the work |
+| `daysWithAudibleReminder` | `[]` | Days of the week that the reminder should display a sound. If  `status = true` and the sound option `daysWithAudibleReminder = []` is empty, the sound will be displayed following the value in `days` |
+| `src` | `done-for-you.mp3` | Name or the url of the mp3 file. By default, the sound of the config will be use if this option is empty. |
 
 
-## Default Alarm Sound
+## Default Sound
 
-The default alarm sound for this module is:
+The alarm sound for this module is `done-for-you.mp3`. However, additional sounds can be placed on the `sounds/` folder:
 
 * [done-for-you.mp3](https://notificationsounds.com/notification-sounds/done-for-you-612) | From Notification Sounds licensed under [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)
+
